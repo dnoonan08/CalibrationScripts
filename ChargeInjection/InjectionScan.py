6e26767eaa04234b11365ec5fea27ce712dc4583
Danny Noonan <dnoonan@cern.ch>
2015-08-09 14:52:43 +0200

    Add functions to injection scans

--- ChargeInjection/InjectionScan.py
+++ ChargeInjection/InjectionScan.py
@@ -6,8 +6,8 @@ import subprocess
 import sys
 
 
-sys.path.insert(0, 'hcal_teststand_scripts')
-sys.path.insert(0, 'ChargeInjectionScripts')
+sys.path.insert(0, '../../hcal_teststand_scripts')
+sys.path.insert(0, '../../ChargeInjectionScripts')
 
 from hcal_teststand.uhtr import *
 from hcal_teststand import *
@@ -16,48 +16,31 @@ from hcal_teststand.qie import *
 
 from scans import *
 
-#from scans import *
-
-parser = OptionParser()
-parser.add_option("-c", "--card", dest="cardnumber", default="2" ,
-                  help="qie card number" )
-parser.add_option("-d", "--dac", dest="dacnumber", default="2" ,
-                  help="DAC used" )
-parser.add_option("-s", "--scan", dest="scan", default="custom" ,
-                  help="choose which range you would like to scan: 0, 1, 2, 3, custom" )
-
-(options, args) = parser.parse_args()
-
-fixRange = options.scan
-
-ts = teststand("904")
-
-print 'loaded ts'
-
-ldlibrarypath = subprocess.Popen('echo $LD_LIBRARY_PATH',stdout=PIPE,shell=True).communicate()[0]
-print ldlibrarypath
-hasUsrLocalLib = '/usr/local/lib' in ldlibrarypath
-hasNGFEC = '/nfshome0/hcalpro/ngFEC' in ldlibrarypath
-
-if not hasUsrLocalLib and not hasNGFEC:
-	os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/nfshome0/hcalpro/ngFEC")
-elif hasNGFEC and not hasUsrLocalLib:
-	os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib")
-elif not hasNGFEC and hasUsrLocalLib:
-	os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/nfshome0/hcalpro/ngFEC")
-
-ldlibrarypath = subprocess.Popen('echo $LD_LIBRARY_PATH',stdout=PIPE,shell=True).communicate()[0]
-print ldlibrarypath
 
 
-path = subprocess.Popen('echo $PATH',stdout=PIPE,shell=True).communicate()[0]
-print path
-if not '/nfshome0/hcalpro/ngFEC' in path:
-	os.system("export PATH=$PATH:/nfshome0/hcalpro/ngFEC")
-path = subprocess.Popen('echo $PATH',stdout=PIPE,shell=True).communicate()[0]
-print path
+# ldlibrarypath = subprocess.Popen('echo $LD_LIBRARY_PATH',stdout=PIPE,shell=True).communicate()[0]
+# hasUsrLocalLib = '/usr/local/lib' in ldlibrarypath
+# hasNGFEC = '/nfshome0/hcalpro/ngFEC' in ldlibrarypath
 
-def setup(range=0):
+# if not hasUsrLocalLib and not hasNGFEC:
+# 	os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/nfshome0/hcalpro/ngFEC")
+# elif hasNGFEC and not hasUsrLocalLib:
+# 	os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib")
+# elif not hasNGFEC and hasUsrLocalLib:
+# 	os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/nfshome0/hcalpro/ngFEC")
+
+# ldlibrarypath = subprocess.Popen('echo $LD_LIBRARY_PATH',stdout=PIPE,shell=True).communicate()[0]
+# print ldlibrarypath
+
+
+# path = subprocess.Popen('echo $PATH',stdout=PIPE,shell=True).communicate()[0]
+# print path
+# if not '/nfshome0/hcalpro/ngFEC' in path:
+# 	os.system("export PATH=$PATH:/nfshome0/hcalpro/ngFEC")
+# path = subprocess.Popen('echo $PATH',stdout=PIPE,shell=True).communicate()[0]
+# print path
+
+def setup(ts, range=0, useFixRange=True, useCalibrationMode=True):
 	commands = [
 		"get HF1-bkp_pwr_bad",
 		"put HF1-bkp_reset 1",
@@ -73,70 +56,86 @@ def setup(range=0):
 		"get HF1-2-iBot_StatusReg_PLL320MHzLock",
 		"quit",
 		]
-
+	
 	commands[4] = commands[4].replace('x',str(range))
+	if not useFixRange:
+		commands[6] = commands[6].replace('24*1','24*0')
+	if not useCalibrationMode:
+		commands[8] = commands[8].replace('24*1','24*0')
 
 	output = ngccm.send_commands_parsed(ts, commands)
 	for line in output['output']:
 		print line['cmd'], line['result']
 
-
-# 	output = ngccm.send_commands_parsed(ts, commands[:4])
-# 	for line in output['output']:
-# 		print line['cmd'], line['result']
-
-# 	output = ngccm.send_commands_parsed(ts, commands[4:8])
-# 	for line in output['output']:
-# 		print line['cmd'], line['result']
-
-# 	output = ngccm.send_commands_parsed(ts, commands[8:])
-# 	for line in output['output']:
-# 		print line['cmd'], line['result']
-
 def setDAC( dacLSB = 0, dacChannel = -1):
 	os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib; ~/../tiroy/mcc-libhid/dacQinjector  -o {0} -c {1}".format(dacLSB, dacChannel) )
 
-outputDirectory = "injector_card_{0}_DAC_{1}/Cal_range_{2}_mode/{3}/".format(options.cardnumber, options.dacnumber,options.scan, str(date.today() ) )
-os.system( "mkdir -pv "+outputDirectory )
+def initLinks(ts):
+	cmds = [
+		'0',
+		'link',
+		'init',
+		'1',
+		'92',
+		'0',
+		'0',
+		'0',
+		'quit',
+		'quit',
+		'exit',
+		'exit'
+		]
+	output = send_commands_script(ts, ts.uhtr_slots[0], cmds)
+	for line in output['output']:
+		print line['cmd'], line['result']
 
-print 'start setup'
 
-setup("{0}".format(options.scan))
+def doScan(ts, injCardNumber = 1, dacNumber = 1, scanRange = range(30), qieRange=0, useFixRange=True, useCalibrationMode=True, saveHistograms = True):
 
-print 'done setup'
 
-scanRange = scans[options.scan]
-scanRange = range(0,51,5)
-scanRange = [100]
-print scanRange
 
-cmds = [
-	'0',
-	'link',
-	'init',
-	'1',
-	'92',
-	'0',
-	'0',
-	'0',
-	'quit',
-	'quit',
-	'exit',
-	'exit'
-	]
-result = send_commands_script(ts, ts.uhtr_slots[0], cmds)
+	if saveHistograms:
+		outputDirectory = "injector_card_{0}_DAC_{1}/Cal_range_{2}_mode/{3}/".format(injCardNumber, dacNumber, qieRange, str(date.today() ) )
+		os.system( "mkdir -pv "+outputDirectory )
 
- 
-for i in scanRange:
-	print i
-	setDAC( i)
-#        setup("{0}".format(options.scan))
-	sleep(5)
-	histName = "Calibration_qiecard_{0}_LSB_{1}.root".format( options.cardnumber, i )
-	fName = uhtr.get_histo(ts,uhtr_slot=ts.uhtr_slots[0],n_orbits=4000, sepCapID=0, file_out=outputDirectory+histName)
-	vals = uhtr.read_histo(fName)
-#	print vals
-	#    saveHisto( outputDirectory+"Calibration_qiecard_{0}_LSB_{1}.root".format( options.cardnumber, i ) )
+	setup(ts,"{0}".format(qieRange))
+
+	initLinks(ts)
+
+	results = {}
+	for i in scanRange:
+		print i
+		setDAC( i)
+		# setup("{0}".format(options.scan))
+		sleep(5)
+		histName = ""
+		if saveHistograms: histName = "Calibration_qiecard_{0}_LSB_{1}.root".format( cardNumber, i )
+		fName = uhtr.get_histo(ts,uhtr_slot=ts.uhtr_slots[0],n_orbits=4000, sepCapID=0, file_out=outputDirectory+histName)
+		vals = uhtr.read_histo(fName)
+		results[i] = vals
 	
-setDAC(0)
-setup("{0}".format(options.scan))            
+	setDAC(0)
+	setup("{0}".format(qieRange))            
+
+
+if __name__ == "__main__":
+	parser = OptionParser()
+	parser.add_option("-c", "--card", dest="cardnumber", default="2" ,
+			  help="qie card number" )
+	parser.add_option("-d", "--dac", dest="dacnumber", default="2" ,
+			  help="DAC used" )
+	parser.add_option("-r", "--range", dest="range",default=0 ,
+			  help="choose which range you would like to scan: 0, 1, 2, 3, custom" )
+	parser.add_option("-s", "--scan", dest="scan", default=[] ,
+			  help="set a custom DAC range you want to scan over" )
+	parser.add_option("--nofixed", action="store_false", dest="useFixedRange", default=True ,
+			  help="do not use fixed range mode")
+	parser.add_option("--nocalmode", action="store_false", dest="useCalibrationMode", default=True ,
+			  help="do not use calibration mode" )
+	parser.add_option("--save","--savehistos",action="store_true",dest="saveHistograms", default=False,
+			  help="save the histogram output files")
+	
+	(options, args) = parser.parse_args()
+	
+	ts = teststand("904")
+
--- ChargeInjection/scans.py
+++ ChargeInjection/scans.py
@@ -1,35 +1,23 @@
-#DACscan = [ 0.0001 , 0.0002 , 0.0003 , 0.0004 , 0.0005 , 0.0006 , 0.0007 , 0.0008 , 0.0009 ,
- #           0.0010 , 0.0020 , 0.0030 , 0.0040 , 0.0050 , 0.0060 , 0.0070 , 0.0080 , 0.0090 ,
-  #          0.0100 , 0.0200 , 0.0300 , 0.0400 , 0.0500 , 0.0600 , 0.0700 , 0.0800 , 0.0900 ,
-   #         0.1000 , 0.2000 , 0.3000 , 0.4000 , 0.5000 , 0.6000 , 0.7000 , 0.8000 , 0.9000 ,
-    #        1.0000 , 2.0000 , 3.0000 , 4.0000 , 5.0000 ]
-DACscan = []
-for i in range(30000):
-	DACscan.append(i)
 
 DACscan_range0 = range(160)
 DACscan_range1 = range(100,240, 5)+ range(240,500,10) + range(500,1000,20) + range(1000,1600,50)
 DACscan_range2 = range(1200,2100, 75)+range(2100,4200,150) + range(4200,9000,300) + range(9000,12000,500)
 DACscan_range3 = range(12000,14000,1000)
 
-# for i in range(0, 130, 1):
-#     DACscan_range0.append(i)
-# for i in range(130,480,10):
-#     DACscan_range0.append(i)
-# DACscan_range1 = []
-# for i in range(125, 4000 , 30): # run scan from 125
-#     DACscan_range1.append(i)
-# DACscan_range3 = []
-# DACscan_range2 = []
-# for i in range(4000, 6200 , 100 ):
-#     DACscan_range2.append(i)
-# for i in range(6200, 34000, 2000):
-#     DACscan_range3.append(i)
-
-
 scans = { "0" : DACscan_range0 ,
           "1" : DACscan_range1 ,
           "2" : DACscan_range2 ,
           "3" : DACscan_range3 ,
-          "custom" : DACscan }
+	  }
 
+DACscan_range0_5k = range(40) + range(40,130,2) + range(130,480,5)
+DACscan_range1_5k = range(350,700, 10)+ range(700,1500,20) + range(1500,4300,40)
+DACscan_range2_5k = range(3400,6100, 75)+range(6100,12300,150) + range(12300,35000,300)
+DACscan_range3_5k = range(28000,45000,1000)
+
+scans5k = { "0" : DACscan_range0_5k ,
+	    "1" : DACscan_range1_5k ,
+	    "2" : DACscan_range2_5k ,
+	    "3" : DACscan_range3_5k ,
+	    }
+	   
