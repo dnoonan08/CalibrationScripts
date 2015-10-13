57fbfc142a0532f0342b62b95f626d6e82c6dd93
Danny Noonan <dnoonan@cern.ch>
2015-10-13 17:23:25 +0200

    First commit

---
+++ ChargeInjection/DAC.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import subprocess
+from time import sleep
+
+def setDAC( dacLSB = 0, dacChannel = -1):
+	os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib; ~/../tiroy/mcc-libhid/dacQinjector  -o {0} -c {1}".format(dacLSB, dacChannel) )
+	sleep(3)
+
+
+def getDACNumber():
+	count = 0
+	foundDevice = False
+	while not foundDevice and count < 5:
+		command = "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib; ~/../tiroy/mcc-libhid/dacQinjector  -o 0"
+		output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).communicate()[0]
+
+		outLines = output.split('\n')
+		foundDevice = False
+		dacNum = '00'
+		for line in outLines:
+			if 'USB 3105 Device is found' in line:
+				foundDevice = True
+			if 'DAC Serial Number' in line:
+				number = line.split()[-1]
+				if number == '00101659':
+					dacNum = '01'
+				if number == '00104076':
+					dacNum = '02'				
+				if number == 'Error':
+					foundDevice = False
+					count += 1
+					print outLines
+	if not foundDevice:
+		print "!!! Unable to find USB DAC !!!"
+		print "Exiting"
+		print count
+		#sys.exit()
+	if dacNum == '00':
+		print "Unknown DAC"
+		print "Exiting"
+		print outLines
+		#sys.exit()
+
+	print 'Using DAC %s' %dacNum
+	return dacNum
+
