6d10ae0cb4272913bf13013665ce8a7c1f0da76c
nstrobbe <nadja.strobbe@cern.ch>
2016-06-04 19:38:09 -0500

    Add serial numbers for new DACs

--- ChargeInjection/DAC.py
+++ ChargeInjection/DAC.py
@@ -3,8 +3,11 @@ import sys
 import subprocess
 from time import sleep
 
-def setDAC( dacLSB = 0, dacChannel = -1):
-	os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib; ~/../tiroy/mcc-libhid/dacQinjector  -o {0} -c {1}".format(dacLSB, dacChannel) )
+def setDAC( dacLSB = 0, dacChannel = -1,relayOn = False):
+        if relayOn==True:
+                os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib; /home/hcalpro/dnoonan/mcc-libhid/dacQinjector -dOn -o {0} -c {1}".format(dacLSB, dacChannel) )
+        else:
+                os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib; /home/hcalpro/dnoonan/mcc-libhid/dacQinjector  -dOff -o {0} -c {1}".format(dacLSB, dacChannel) )
 	sleep(2)
 
 
@@ -12,7 +15,7 @@ def getDACNumber():
 	count = 0
 	foundDevice = False
 	while not foundDevice and count < 5:
-		command = "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib; ~/../tiroy/mcc-libhid/dacQinjector  -o 0"
+		command = "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib; /home/hcalpro/dnoonan/mcc-libhid/dacQinjector -o 0 -verbose"
 		output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).communicate()[0]
 
 		outLines = output.split('\n')
@@ -25,9 +28,17 @@ def getDACNumber():
 				number = line.split()[-1]
 				if number == '00101659':
 					dacNum = '01'
-				if number == '00104076':
-					dacNum = '02'				
-				if number == 'Error':
+				elif number == '00104076':
+					dacNum = '02'
+                                elif number == '00108124':
+                                        dacNum = '03'
+                                elif number == '00108645':
+                                        dacNum = '04'
+                                elif number == '00108644':
+                                        dacNum = '05'
+                                elif number == '00108646':
+                                        dacNum = '06'
+                                elif number == 'Error':
 					foundDevice = False
 					count += 1
 					print outLines
@@ -52,3 +63,6 @@ if __name__=='__main__':
 
 	if len(sys.argv)==3:
 		setDAC(sys.argv[1],sys.argv[2])
+
+	if len(sys.argv)==4:
+		setDAC(sys.argv[1],sys.argv[2],eval(sys.argv[3]))
